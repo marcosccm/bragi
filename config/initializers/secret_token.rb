5286d61a1e2ef4268fb0f8579c910fd7e032b6fc
marcosccm <marcosccm@gmail.com>
2011-03-27 23:30:31 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bragi::Application.config.secret_token = 'b485b9bca7a4b3d47ac3b5328c689996afd7529bf69bc6e6de52b5ae4d61646ea0af3668271412bf4fd1c8411116e9da679e51f4aaa1bc954657581a5cd77b3c'
